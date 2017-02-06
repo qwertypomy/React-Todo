4cf5430b88016ec7ac3c8faa3cd40ebde6f4ad1a
qwertypomy <qwertypomy@gmail.com>
2017-02-06 15:24:45 +0200

    Refactor TodoList, Todo, and AddTodo to work with redux

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
+
+var configureStore = require('configureStore');
+var TodoApp = require('TodoApp');
+import TodoList from 'TodoList'
 
 var TodoApp = require('TodoApp');
 
@@ -11,46 +16,17 @@ describe('TodoApp', () => {
     expect(TodoApp).toExist();
   });
 
-  it('should add todo to the todos state on handleAddTodo', () => {
-    var todoText = 'test text';
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
-    todoApp.setState({todos: []});
-    todoApp.handleAddTodo(todoText);
+  it('should render TodoList', () => {
+    var store = configureStore.configure();
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <TodoApp/>
+      </Provider>
+    );
 
-    expect(todoApp.state.todos[0].text).toBe(todoText);
-    expect(todoApp.state.todos[0].createdAt).toBeA('number');
-  });
+    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0];
+    var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
 
-  it('should toggle completed value when handleToggle called', () => {
-    var todoData = {
-      id: 11,
-      text: 'Test features',
-      completed: false,
-      createdAt: 0,
-      completedAt: undefined
-    };
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
-    todoApp.setState({todos: [todoData]});
-
-    expect(todoApp.state.todos[0].completed).toBe(false);
-    todoApp.handleToggle(11);
-    expect(todoApp.state.todos[0].completed).toBe(true);
-    expect(todoApp.state.todos[0].completedAt).toBeA('number');
-  });
-
-  it('should toggle todo from completed to incompleted', () => {
-    var todoData = {
-      id: 11,
-      text: 'Test features',
-      completed: true,
-      createdAt: 0,
-      completedAt: 1
-    };
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
-    todoApp.setState({todos: [todoData]});
-    expect(todoApp.state.todos[0].completed).toBe(true);
-    todoApp.handleToggle(11);
-    expect(todoApp.state.todos[0].completed).toBe(false);
-    expect(todoApp.state.todos[0].completedAt).toNotExist();
+    expect(todoList.length).toEqual(1);
   });
 });
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
-var TodoList = require('TodoList');
-var Todo = require('Todo');
+import {configure} from 'configureStore';
+import ConnectedTodoList, {TodoList} from 'TodoList';
+import ConnectedTodo, {Todo} from 'Todo'
 
 describe('TodoList', () => {
   it('should exist', () => {
@@ -14,16 +16,30 @@ describe('TodoList', () => {
 
   it('should render one Todo component for each todo item', () => {
     var todos = [{
-      id:1,
-      text: "Do something"
+      id: 1,
+      text: 'Do something',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 500
     }, {
-      id:2,
-      text: 'Check mail'
+      id: 2,
+      text: 'Check mail',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 500
     }];
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var todosComponens = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    var store = configure({
+      todos
+    });
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <ConnectedTodoList/>
+      </Provider>
+    );
+    var todoList = TestUtils.scryRenderedComponentsWithType(provider, ConnectedTodoList)[0];
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ConnectedTodo);
 
-    expect(todosComponens.length).toBe(todos.length);
+    expect(todosComponents.length).toBe(todos.length);
   });
 
   it('should render empty message if no todos', () => {
